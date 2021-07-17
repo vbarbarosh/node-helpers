2c482a797306b60af43d528ddc9337f5d30d4d98
Vladimir Barbarosh <vladimir.barbarosh@gmail.com>
2021-07-18 02:28:40 +0300

    add throttle

---
+++ src/throttle.js
@@ -0,0 +1,30 @@
+/**
+ * Ensure that only one event was fired for each ms.
+ *
+ * @link https://stackoverflow.com/questions/25991367/difference-between-throttling-and-debouncing-a-function
+ */
+function throttle(ms, fn)
+{
+    let timer = null;
+    let last_value = undefined;
+    run.fire = fire;
+    return run;
+    function run(value) {
+        last_value = value;
+        if (timer === null) {
+            timer = setTimeout(fire, ms)
+        }
+    }
+    function fire() {
+        if (timer === null) {
+            return;
+        }
+        clearTimeout(timer);
+        timer = null;
+        const tmp = last_value;
+        last_value = undefined;
+        return fn(tmp);
+    }
+}
+
+export default throttle;
