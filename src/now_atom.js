9067b3883858eef167f1b693419c14b823cdbf3f
Vladimir Barbarosh <vladimir.barbarosh@gmail.com>
2021-07-18 02:28:03 +0300

    add now_atom

---
+++ src/now_atom.js
@@ -0,0 +1,12 @@
+/**
+ * Return current date in ATOM format (e.g. "2021-07-17T23:27:01.030Z")
+ *
+ * @returns {string}
+ * @link https://www.php.net/manual/en/class.datetimeinterface.php#datetime.constants.atom
+ */
+function now_atom()
+{
+    return (new Date()).toJSON();
+}
+
+export default now_atom;
