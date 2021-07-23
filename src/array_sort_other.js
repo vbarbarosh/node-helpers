c33b44fa0c9eaa9238177146d582522fb3805a0d
Vladimir Barbarosh <vladimir.barbarosh@gmail.com>
2021-07-23 11:11:46 +0300

    rename array_sort_by_keys to array_sort_other

---
+++ src/array_sort_other.js
@@ -0,0 +1,33 @@
+import fcmp_strings from './fcmp_strings';
+
+/**
+ * Sort items in an `array` at the same order as in `other`. Values which
+ * are not in the `other` are added to the end of the result defined by `fcmp`.
+ *
+ * @param array
+ * @param fn
+ * @param other
+ * @param fcmp
+ * @returns {*}
+ */
+function array_sort_other(array, fn, other, fcmp = fcmp_strings)
+{
+    const other_map = {};
+    other.forEach((v,i) => other_map[v] = i + 1);
+    return array.sort(function (a, b) {
+        const ax = other_map[fn(a)];
+        const bx = other_map[fn(b)];
+        if (ax && bx) {
+            return ax - bx;
+        }
+        if (ax) {
+            return -1;
+        }
+        if (bx) {
+            return 1;
+        }
+        return fcmp(a, b);
+    });
+}
+
+export default array_sort_other;
