659d126d19e9826fd37784d9c0541e91afd30bd0
Vladimir Barbarosh <vladimir.barbarosh@gmail.com>
2021-12-04 04:13:06 +0200

    demos/http_server_echo: handle /redirect path

--- demos/http_server_echo/index.js
+++ demos/http_server_echo/index.js
@@ -6,6 +6,15 @@ const http = require('http');
 const server = http.createServer(function (req, res) {
     console.log(req.method, req.url, req.headers);
     console.log();
+
+    const url = new URL(`http://localhost:3000${req.url}`);
+    if (url.pathname == '/redirect' && url.searchParams.has('url')) {
+        res.statusCode = 302;
+        res.setHeader('Location', url.searchParams.get('url'));
+        res.end();
+        return;
+    }
+
     res.statusCode = 200;
     req.headers['content-type'] && res.setHeader('Content-Type', req.headers['content-type']);
     req.headers['content-length'] && res.setHeader('Content-Length', req.headers['content-length']);
