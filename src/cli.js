691452b4c4571e8632f7384d62bf9f96bb54e726
Vladimir Barbarosh <vladimir.barbarosh@gmail.com>
2021-12-04 04:18:08 +0200

    cli is no more async function

--- src/cli.js
+++ src/cli.js
@@ -1,38 +1,21 @@
 /**
  * Entry point for Node CLI apps
- *
- * @link https://github.com/vbarbarosh/node-cli
  */
-async function cli(main)
+function cli(main)
 {
     // https://stackoverflow.com/a/46916601/1478566
     const timer = setInterval(v => v, 1E9);
 
-    try {
-        return await main();
+    Promise.resolve(main()).then(resolve, reject);
+
+    function resolve() {
+        clearInterval(timer);
     }
-    catch (error) {
+    function reject(error) {
+        clearInterval(timer);
         console.error(error);
         process.exit(1);
     }
-    finally {
-        clearInterval(timer);
-    }
 }
-
-// import Promise from 'bluebird';
-//
-// // https://stackoverflow.com/a/57241059/1478566
-// function cli(main)
-// {
-//     // https://stackoverflow.com/a/46916601/1478566
-//     return Promise.method(main).call().catch(panic).finally(clearInterval.bind(null, setInterval(v=>v, 1E9)));
-// }
-//
-// function panic(error)
-// {
-//     console.error(error);
-//     process.exit(1);
-// }
 
 export default cli;
