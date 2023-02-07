482294286e40c1644e5d139dcf27f1afbcdf2921
Vladimir Barbaros <vladimir.b@bannernow.com>
2023-02-07 20:36:01 +0200

    add shell_spawn helper

---
+++ src/shell_spawn.js
@@ -0,0 +1,33 @@
+const child_process = require('child_process');
+
+function shell_spawn(args, options)
+{
+    const proc = child_process.spawn(args[0], args.slice(1), options);
+    let promise;
+    proc.promise = function () {
+        let is_pending = true;
+        return promise = promise || new Promise(function (resolve, reject) {
+            proc.on('error', on_error);
+            proc.on('exit', on_exit);
+            function on_error(error) {
+                if (is_pending) {
+                    is_pending = false;
+                    proc.off('error', on_error);
+                    proc.off('exit', on_exit);
+                    reject(error);
+                }
+            }
+            function on_exit(code, signal) {
+                if (is_pending) {
+                    is_pending = false;
+                    proc.off('error', on_error);
+                    proc.off('exit', on_exit);
+                    resolve({code, signal});
+                }
+            }
+        });
+    };
+    return proc;
+}
+
+module.exports = shell_spawn;
