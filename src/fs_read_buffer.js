819dd11b7ceb3d57eddabbde0400944145510e8b
Vladimir Barbaros <vladimir.b@bannernow.com>
2023-02-12 18:04:10 +0200

    clean

--- src/fs_read_buffer.js
+++ src/fs_read_buffer.js
@@ -1,8 +1,8 @@
 const fs_read = require('./fs_read');
 
-async function fs_read_buffer(file)
+function fs_read_buffer(file)
 {
-    return await fs_read(file);
+    return fs_read(file);
 }
 
 module.exports = fs_read_buffer;
--- src/fs_read_utf8.js
+++ src/fs_read_utf8.js
@@ -1,8 +1,8 @@
 const fs_read = require('./fs_read');
 
-async function fs_read_utf8(file)
+function fs_read_utf8(file)
 {
-    return await fs_read(file, {encoding: 'utf8'});
+    return fs_read(file, {encoding: 'utf8'});
 }
 
 module.exports = fs_read_utf8;
