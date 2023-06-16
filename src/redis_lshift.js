dc7136d0218e6f498ae8a47baa773b1cfe4e1f0b
Vladimir Barbaros <vladimir.b@bannernow.com>
2023-06-17 00:12:13 +0300

    update redis_lshift.js redis_zshift.js

--- src/redis_lshift.js
+++ src/redis_lshift.js
@@ -1,7 +1,12 @@
 async function redis_lshift(redis, queue, limit = 1)
 {
     const last = limit - 1;
-    const [items] = await redis.multi().lrange(queue, 0, last).ltrim(queue, limit, -1).exec_p();
+    if (redis.lrange) {
+        const [items] = await redis.multi().lrange(queue, 0, last).ltrim(queue, limit, -1).exec_p();
+        return items;
+    }
+    // redis@4
+    const [items] = await redis.multi().LRANGE(queue, 0, last).LTRIM(queue, limit, -1).exec();
     return items;
 }
 
--- src/redis_zshift.js
+++ src/redis_zshift.js
@@ -1,8 +1,12 @@
 async function redis_zshift(redis, queue, limit = 1)
 {
     const last = limit - 1;
-    // noinspection JSUnresolvedFunction
-    const [items] = await redis.multi().zrange(queue, 0, last).zremrangebyrank(queue, 0, last).exec_p();
+    if (redis.zrange) {
+        const [items] = await redis.multi().zrange(queue, 0, last).zremrangebyrank(queue, 0, last).exec_p();
+        return items;
+    }
+    // redis@4
+    const [items] = await redis.multi().ZRANGE(queue, 0, last).ZREMRANGEBYRANK(queue, 0, last).exec();
     return items;
 }
 
