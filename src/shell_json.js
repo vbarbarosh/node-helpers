d009092329c7fc38dd545fed67648f1b0ddb119b
Vladimir Barbaros <vladimir.b@bannernow.com>
2023-09-11 20:36:02 +0300

    add shell_json

---
+++ src/shell_json.js
@@ -0,0 +1,25 @@
+const child_process = require('child_process');
+
+function shell_json(args, options)
+{
+    return new Promise(function (resolve, reject) {
+        child_process.execFile(args[0], args.slice(1), options, function (error, stdout, stderr) {
+            if (error) {
+                reject(error);
+            }
+            else if (stderr) {
+                reject(new Error(`Process terminated with the following stderr:\n\n${stderr}`));
+            }
+            else {
+                try {
+                    resolve(JSON.parse(stdout));
+                }
+                catch (error) {
+                    reject(error);
+                }
+            }
+        });
+    });
+}
+
+module.exports = shell_json;
