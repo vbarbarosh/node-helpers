65586bd02fd5d22b20518105c3e30947849bf114
Vladimir Barbaros <vladimir.b@bannernow.com>
2023-12-25 23:07:02 +0200

    update fs_write

--- src/fs_write.js
+++ src/fs_write.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
-const util = require('util');
 
-// https://nodejs.org/api/util.html#util_util_promisify_original
-module.exports = util.promisify(fs.writeFile);
+async function fs_write(file, data, options = {})
+{
+    return fs.promises.writeFile(file, data, options);
+}
+
+module.exports = fs_write;
