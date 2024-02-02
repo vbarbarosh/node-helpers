414f8e537576740e13fd8390161a6cd6a71d1fbb
Vladimir Barbaros <vladimir.b@bannernow.com>
2024-02-02 22:55:44 +0200

    eta: fix

--- src/eta.js
+++ src/eta.js
@@ -3,10 +3,9 @@ const format_seconds = require('./format_seconds');
 function eta(time0, total, done, resumed = 0)
 {
     const seconds = (Date.now() - time0)/1000;
-    const xdone = done - resumed;
-    const xtotal = total - resumed;
-    const bps = xdone/seconds;
-    return format_seconds(xtotal/bps);
+    const remained = total - done;
+    const bps = (done - resumed)/seconds;
+    return format_seconds(remained/bps);
 }
 
 module.exports = eta;
