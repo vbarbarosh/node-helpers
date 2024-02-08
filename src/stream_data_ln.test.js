3f4b0f36dca11df9cc22a6cdf9ab3a6037e64faa
Vladimir Barbaros <vladimir.barbarosh@gmail.com>
2024-02-08 19:28:21 +0200

    test with split lib

--- src/stream_data_ln.test.js
+++ src/stream_data_ln.test.js
@@ -1,15 +1,25 @@
 const assert = require('assert');
 const fs = require('fs');
+const split = require('split');
+const stream = require('stream');
 const stream_data_ln = require('./stream_data_ln');
 const stream_promise = require('./stream_promise');
 
 describe('stream_data_ln', function () {
     it('should handle basic input', async function () {
-        let tmp = '';
+        let actual = '';
         const s = fs.createReadStream(__filename);
-        const off = stream_data_ln(s, v => tmp += v + '\n');
+        const off = stream_data_ln(s, v => actual += v + '\n');
         await stream_promise(s);
         off();
-        assert.deepStrictEqual(tmp, fs.readFileSync(__filename, {encoding: 'utf8'}));
+        assert.deepStrictEqual(actual, fs.readFileSync(__filename, {encoding: 'utf8'}));
+    });
+    it('split', async function () {
+        let actual = '';
+        const s = fs.createReadStream(__filename);
+        const ss = s.pipe(split('\n', null, {trailing: false}));
+        ss.on('data', v => actual += v + '\n');
+        await stream.promises.finished(ss);
+        assert.deepStrictEqual(actual, fs.readFileSync(__filename, {encoding: 'utf8'}));
     });
 });
