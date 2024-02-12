916865f2feca855935dc7be4f6131b90808eb26f
Vladimir Barbaros <vladimir.barbarosh@gmail.com>
2024-02-12 22:37:10 +0200

    progress: determine speed from history of last 5 seconds

--- demos/progress/index.js
+++ demos/progress/index.js
@@ -12,6 +12,6 @@ async function main()
     for (let i = 0; i < 1000; ++i) {
         await Promise.delay(Math.random()*25);
         p.add(1);
-        console.log(`${p.done} of ${p.total} ${(p.percentage*100).toFixed(2)}% rate=${p.rate.toFixed(0)}/s eta=${p.eta.toFixed(2)}s duration=${p.duration.toFixed(2)}s`);
+        console.log(`${p.done} of ${p.total} ${(p.percentage*100).toFixed(2)}% at ${p.rate.toFixed(2)}/s ETA ${p.eta.toFixed(2)}s duration=${p.duration.toFixed(2)}s`);
     }
 }
--- src/progress.js
+++ src/progress.js
@@ -8,27 +8,41 @@
 // https://bramcohen.livejournal.com/24122.html
 function progress(total)
 {
-    let done = 0;
+    const history = [];
     const time0 = Date.now();
     const out = {
         done: 0,
         total,
         eta: null,
+        // `rate` instead of `bps` (bytes per speed) because this function could count any values (e.g.
+        // copied bytes over time, or handled jobs over time, or created object over time, etc.)
         rate: null,
         duration: 0,
         progress: null,
         percentage: null,
         add: function (delta = 0) {
+            const now_minus_5_seconds = Date.now() - 5000;
+            history.push({time: Date.now(), delta});
+            // Keep last 5 seconds
+            history.splice(0, history.findLastIndex(v => v.time < now_minus_5_seconds));
             out.done += delta;
             out.refresh();
         },
         update: function (done) {
-            out.done = done;
-            out.refresh();
+            out.add(done - out.done);
         },
         refresh: function () {
+            if (history.length > 1) {
+                const now_minus_5_seconds = Date.now() - 5000;
+                const tmp = history.filter(v => v.time >= now_minus_5_seconds);
+                const items = tmp.reduce((a,v) => a + v.delta, 0);
+                const time_sec = (tmp.pop().time - tmp[0].time)/1000;
+                out.rate = items/time_sec;
+            }
+            else {
+                out.rate = out.done/out.duration;
+            }
             out.duration = (Date.now() - time0)/1000;
-            out.rate = out.done/out.duration;
             out.percentage = !out.total ? null : out.done/out.total;
             out.eta = !out.total ? null : (out.total - out.done)/out.rate;
         },
