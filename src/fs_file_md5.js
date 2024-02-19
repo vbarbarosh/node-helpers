4a4da5bbbe7e7a3c9b05bb5916421bfc356d68b0
Vladimir Barbaros <vladimir.barbarosh@gmail.com>
2024-02-19 13:37:48 +0200

    add fs_file_md5

---
+++ src/fs_file_md5.js
@@ -0,0 +1,12 @@
+const crypto = require('crypto');
+const fs = require('fs');
+
+async function fs_file_md5(file, encoding = 'hex')
+{
+    const md5 = crypto.createHash('md5');
+    const rs = fs.createReadStream(file);
+    await rs.forEach(v => md5.update(v));
+    return md5.digest(encoding);
+}
+
+module.export = fs_file_md5;
