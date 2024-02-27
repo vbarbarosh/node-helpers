b5151fd6cf4a93d1f8bf6f2881dad18a35e69f8b
Vladimir Barbaros <vladimir.barbarosh@gmail.com>
2024-02-27 20:16:12 +0200

    add stream_group

---
+++ src/stream_group.js
@@ -0,0 +1,27 @@
+const stream = require('stream');
+
+/**
+ * Returns a transform stream used to group object into chunks of objects.
+ */
+function stream_group(chunk_size)
+{
+    const chunk = [];
+    return new stream.Transform({
+        objectMode: true,
+        transform: function (item, enc, next) {
+            chunk.push(item);
+            if (chunk.length >= chunk_size) {
+                this.push(chunk.splice(0));
+            }
+            next();
+        },
+        flush: function (next) {
+            if (chunk.length) {
+                this.push(chunk.splice(0));
+                next();
+            }
+        },
+    });
+}
+
+module.exports = stream_group;
