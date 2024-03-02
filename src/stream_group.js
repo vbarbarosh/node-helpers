764e3142828fecbd64ac94e0d70c610422280c3c
Vladimir Barbaros <vladimir.barbarosh@gmail.com>
2024-03-02 10:19:47 +0200

    stream_group: case when no items was written

--- src/stream_group.js
+++ src/stream_group.js
@@ -8,7 +8,7 @@ function stream_group(chunk_size)
     const chunk = [];
     return new stream.Transform({
         objectMode: true,
-        transform: function (item, enc, next) {
+        transform: function (item, encoding, next) {
             chunk.push(item);
             if (chunk.length >= chunk_size) {
                 this.push(chunk.splice(0));
@@ -18,8 +18,8 @@ function stream_group(chunk_size)
         flush: function (next) {
             if (chunk.length) {
                 this.push(chunk.splice(0));
-                next();
             }
+            next();
         },
     });
 }
