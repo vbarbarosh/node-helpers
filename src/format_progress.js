fc3f0244d15e0e89a9f0aab596e001c7a267c757
Vladimir Barbaros <vladimir.barbarosh@gmail.com>
2024-03-03 17:17:51 +0200

    format_progress: progress

--- src/format_progress.js
+++ src/format_progress.js
@@ -4,10 +4,12 @@ const format_seconds = require('./format_seconds');
 
 function format_progress({percents, total, done, rate, eta, duration})
 {
+    const bps = rate ? `${format_bytes(rate)}/s` : '~';
     if (total) {
-        return `${format_percents(percents)} | ${format_bytes(done)} of ${format_bytes(total)} at ${format_bytes(rate)}/s ETA ${format_seconds(eta)} duration=${format_seconds(duration)}`;
+        const eta_str = eta ? format_seconds(eta) : '~';
+        return `${format_percents(percents)} | ${format_bytes(done)} of ${format_bytes(total)} at ${bps} ETA ${eta_str} duration=${format_seconds(duration)}`;
     }
-    return `${format_bytes(done)} of ~ at ${format_bytes(rate)}/s duration=${format_seconds(duration)}`;
+    return `${format_bytes(done)} of ~ at ${bps} duration=${format_seconds(duration)}`;
 }
 
 module.exports = format_progress;
