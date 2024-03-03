17501265de9eeed149c837d8e36deaf7b086d135
Vladimir Barbaros <vladimir.barbarosh@gmail.com>
2024-03-03 18:45:28 +0200

    format_percents: add 2 decimals except for 0 and 100

--- src/format_percents.js
+++ src/format_percents.js
@@ -1,6 +1,6 @@
 function format_percents(v)
 {
-    return (v*100).toFixed(2).replace(/\.0+$/, '') + '%';
+    return (v*100).toFixed(2).replace(/^(0|100).00$/, '$1') + '%';
 }
 
 module.exports = format_percents;
