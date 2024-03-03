d87d63ad3324fbda3dc4e168ff7d032b4499f382
Vladimir Barbaros <vladimir.barbarosh@gmail.com>
2024-03-03 07:20:39 +0200

    add stream_hash

---
+++ src/stream_hash.js
@@ -0,0 +1,19 @@
+const crypto = require('crypto');
+const stream = require('stream');
+
+function stream_hash(algorithm = 'md5', options)
+{
+    const hash = crypto.createHash(algorithm, options);
+    return new stream.Transform({
+        transform: function (chunk, encoding, next) {
+            hash.update(chunk);
+            next();
+        },
+        flush: function (next) {
+            this.push(hash.digest());
+            next();
+        },
+    });
+}
+
+module.exports = stream_hash;
