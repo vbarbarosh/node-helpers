cd3795574e1b86b3dfea781a2dff66d8535931cc
Vladimir Barbaros <vladimir.barbarosh@gmail.com>
2024-03-03 18:08:54 +0200

    format_progress: edge cases

--- src/format_progress.js
+++ src/format_progress.js
@@ -1,15 +1,22 @@
 const format_bytes = require('./format_bytes');
 const format_percents = require('./format_percents');
 const format_seconds = require('./format_seconds');
+const is_number_gt = require('./is_number_gt');
 
 function format_progress({percents, total, done, rate, eta, duration})
 {
-    const bps = rate ? `${format_bytes(rate)}/s` : '~';
-    if (total) {
-        const eta_str = eta ? format_seconds(eta) : '~';
+    const bps = is_number_gt(rate, 0) ? `${format_bytes(rate)}/s` : '~';
+    if (is_number_gt(total, 0)) {
+        const eta_str = is_number_gt(eta, 0) ? format_seconds(eta) : '~';
         return `${format_percents(percents)} | ${format_bytes(done)} of ${format_bytes(total)} at ${bps} ETA ${eta_str} duration=${format_seconds(duration)}`;
     }
-    return `${format_bytes(done)} of ~ at ${bps} duration=${format_seconds(duration)}`;
+    if (is_number_gt(done, 0)) {
+        return `${format_bytes(done)} of ~ at ${bps} duration=${format_seconds(duration)}`;
+    }
+    if (is_number_gt(duration, 0)) {
+        return `~ duration=${format_seconds(duration)}`;
+    }
+    return '~';
 }
 
 module.exports = format_progress;
---
+++ src/format_progress.test.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+const format_progress = require('./format_progress');
+
+const items = [
+    ['~', {percents: 0, total: 0, done: 0, rate: 0, eta: 0, duration: 0}],
+    ['~ duration=00:00:01', {percents: 0, total: 0, done: 0, rate: 0, eta: 0, duration: 1}],
+    ['25.25% | 5.0MB of 15.0MB at ~ ETA ~ duration=00:00:01', {percents: 0.2525, total: 15*1024*1024, done: 5*1024*1024, rate: 0, eta: 0, duration: 1}],
+    ['25.25% | 5.0MB of 15.0MB at 10.0MB/s ETA 00:00:05 duration=00:00:01', {percents: 0.2525, total: 15*1024*1024, done: 5*1024*1024, rate: 10*1024*1024, eta: 5, duration: 1}],
+];
+
+describe('format_progress', function () {
+    items.forEach(function ([expected, input]) {
+        it(expected, function () {
+            assert.strictEqual(format_progress(input), expected);
+        });
+    });
+});
