37a4277479cee06bf0eb9fa2983f3f4660cf9834
Vladimir Barbaros <vladimir.barbarosh@gmail.com>
2024-03-03 18:32:05 +0200

    format_progress: edge case: done > total

--- src/format_progress.js
+++ src/format_progress.js
@@ -6,6 +6,9 @@ const is_number_gt = require('./is_number_gt');
 function format_progress({percents, total, done, rate, eta, duration})
 {
     const bps = is_number_gt(rate, 0) ? `${format_bytes(rate)}/s` : '~';
+    if (done > total) {
+        return `${format_bytes(done)} at ${bps} duration=${format_seconds(duration)}`;
+    }
     if (is_number_gt(total, 0)) {
         const eta_str = is_number_gt(eta, 0) ? format_seconds(eta) : '~';
         return `${format_percents(percents)} | ${format_bytes(done)} of ${format_bytes(total)} at ${bps} ETA ${eta_str} duration=${format_seconds(duration)}`;
--- src/format_progress.test.js
+++ src/format_progress.test.js
@@ -6,6 +6,8 @@ const items = [
     ['~ duration=00:00:01', {percents: 0, total: 0, done: 0, rate: 0, eta: 0, duration: 1}],
     ['25.25% | 5.0MB of 15.0MB at ~ ETA ~ duration=00:00:01', {percents: 0.2525, total: 15*1024*1024, done: 5*1024*1024, rate: 0, eta: 0, duration: 1}],
     ['25.25% | 5.0MB of 15.0MB at 10.0MB/s ETA 00:00:05 duration=00:00:01', {percents: 0.2525, total: 15*1024*1024, done: 5*1024*1024, rate: 10*1024*1024, eta: 5, duration: 1}],
+    // edge case: done > total
+    ['15.0MB at 10.0MB/s duration=00:00:01', {percents: 5.2525, total: 5*1024*1024, done: 15*1024*1024, rate: 10*1024*1024, eta: 5, duration: 1}],
 ];
 
 describe('format_progress', function () {
