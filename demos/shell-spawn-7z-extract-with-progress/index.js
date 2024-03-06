54faa7c3c43f9dd21f610a27958ad59bfb4ef5d0
Vladimir Barbaros <vladimir.barbarosh@gmail.com>
2024-03-06 10:15:50 +0200

    update demos/shell-spawn-7z-extract-with-progress

--- demos/shell-spawn-7z-extract-with-progress/index.js
+++ demos/shell-spawn-7z-extract-with-progress/index.js
@@ -9,12 +9,20 @@ cli(main);
 async function main()
 {
     const proc = await shell_spawn(['7z', 'x', '-aoa', '-bsp1', '/path/to/file.zip']).init();
-    for await (const line of proc.stdout.pipe(stream_strpbrk('\r\n\x08'))) {
-        const m = line.match(/^\s*(\d+%.*)\s*$/);
-        if (m) {
-            console.log(`Extracting: ${m[1]}`);
+    const lines = [];
+    try {
+        for await (const line of proc.stdout.pipe(stream_strpbrk('\r\n\x08'))) {
+            lines.push(line);
+            const m = line.match(/^\s*(\d+%.*)\s*$/);
+            if (m) {
+                console.log(`Extracting: ${m[1]}`);
+            }
         }
+        await proc.promise();
+        console.log('done');
     }
-    await proc.promise();
-    console.log('done');
+    catch (error) {
+        console.log('FAILED');
+        console.log(`${error.message}\n\n${lines.join('\n')}`);
+    }
 }
