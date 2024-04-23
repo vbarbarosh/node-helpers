0588893c788ca147ad6a5a1f78ef4077a622f9c9
Vladimir Barbaros <vladimir.b@bannernow.com>
2024-04-23 06:51:26 +0300

    stream_progress: support for objectMode

--- src/stream_progress.js
+++ src/stream_progress.js
@@ -7,20 +7,27 @@ const stream = require('stream');
  *
  * @similar https://www.npmjs.com/package/progress-stream
  */
-function stream_progress({total, user_friendly_status = s => console.log(s)} = {})
+function stream_progress({objectMode = false, total, user_friendly_status = s => console.log(s)} = {})
 {
     let done = 0;
     const timer = setInterval(tick, 1000);
     const progress = make_progress(total);
     return new stream.Transform({
+        objectMode,
         destroy: function (error, next) {
             tick();
             clearInterval(timer);
             next();
         },
         transform: function (buf, encoding, next) {
-            done += buf.length;
-            this.push(buf, encoding);
+            if (objectMode) {
+                done++;
+                this.push(buf);
+            }
+            else {
+                done += buf.length;
+                this.push(buf, encoding);
+            }
             next();
         },
     });
