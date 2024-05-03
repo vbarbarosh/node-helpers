1618c5a7daa659a10ddc801c49b4e8146738264c
Vladimir Barbaros <vladimir.b@bannernow.com>
2024-05-03 17:32:48 +0300

    fix

--- src/shell_thru.js
+++ src/shell_thru.js
@@ -1,8 +1,8 @@
-const shell = require('./shell');
+const shell_spawn = require('./shell_spawn');
 
 function shell_thru(args, options)
 {
-    return shell(args, {stdio: 'inherit', ...options});
+    return shell_spawn(args, {stdio: 'inherit', ...options});
 }
 
 module.exports = shell_thru;
