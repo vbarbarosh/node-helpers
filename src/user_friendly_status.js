4de2290167e3690acce8e8b6c95fbfb36026af82
Vladimir Barbaros <vladimir.b@bannernow.com>
2024-06-16 22:25:27 +0300

    add user_friendly_status

---
+++ src/user_friendly_status.js
@@ -0,0 +1,12 @@
+/**
+ * Concept. Represents a way to display a status of a currently running task to the end-user.
+ *
+ * @see countdown
+ * @see progress
+ */
+function user_friendly_status(s)
+{
+    console.log(s);
+}
+
+module.exports = user_friendly_status;
