00d1c5c8e3b7999a7a97a5914b1477a44bde03aa
Vladimir Barbaros <vladimir.b@bannernow.com>
2024-08-25 17:49:44 +0300

    format_bytes: replace .00 by .0

--- src/format_bytes.js
+++ src/format_bytes.js
@@ -11,7 +11,7 @@ function format_bytes(bytes)
         return '1KB';
     }
     const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)), 10);
-    return `${(bytes / (1024 ** i)).toFixed(bytes > 1024*1024 ? 2 : 0)}${sizes[i]}`;
+    return `${(bytes / (1024 ** i)).toFixed(bytes > 1024*1024 ? 2 : 0)}${sizes[i]}`.replace(/\.00/, '.0');
 }
 
 module.exports = format_bytes;
