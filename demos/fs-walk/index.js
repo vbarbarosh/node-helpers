334f9d92fab1d546885e865495a00d90919ed926
Vladimir Barbaros <vladimir.b@bannernow.com>
2024-11-10 20:58:33 +0200

    add demos/fs-walk

---
+++ demos/fs-walk/index.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+const cli = require('../../src/cli');
+const format_progress_kilo = require('../../src/format_progress_kilo');
+const fs_lstat = require('../../src/fs_lstat');
+const fs_path_join = require('../../src/fs_path_join');
+const fs_readdir = require('../../src/fs_readdir');
+const make_progress = require('../../src/make_progress');
+
+cli(main);
+
+async function main()
+{
+    let last_progress = 0;
+
+    const p = make_progress();
+    const pending = ['/usr/share'];
+    while (pending.length) {
+        const path = pending.shift();
+        const lstat = await fs_lstat(path);
+        if (lstat.isDirectory()) {
+            const basenames = await fs_readdir(path);
+            basenames.forEach(v => pending.push(fs_path_join(path, v)));
+        }
+        p.add(1);
+        if (Date.now() - last_progress > 1000) {
+            console.log(format_progress_kilo(p));
+            last_progress = Date.now();
+        }
+    }
+
+    console.log(format_progress_kilo(p));
+    console.log('🎉 Done');
+}
