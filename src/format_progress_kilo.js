b7c0f6436463be29ef3c9c3d11034b3b7688d807
Vladimir Barbaros <vladimir.b@bannernow.com>
2024-11-10 19:59:33 +0200

    add format_progress_kilo

---
+++ src/format_progress_kilo.js
@@ -0,0 +1,25 @@
+const format_kilo = require('./format_kilo');
+const format_percents = require('./format_percents');
+const format_seconds = require('./format_seconds');
+const is_number_gt = require('./is_number_gt');
+
+function format_progress_kilo({percents, total, done, rate, eta, duration})
+{
+    const speed = is_number_gt(rate, 0) ? `${format_kilo(rate)}/s` : '~';
+    if (done > total) {
+        return `${format_kilo(done)} at ${speed} duration=${format_seconds(duration)}`;
+    }
+    if (is_number_gt(total, 0)) {
+        const eta_str = is_number_gt(eta, 0) ? format_seconds(eta) : '~';
+        return `${format_percents(percents)} | ${format_kilo(done)} of ${format_kilo(total)} at ${speed} ETA ${eta_str} duration=${format_seconds(duration)}`;
+    }
+    if (is_number_gt(done, 0)) {
+        return `${format_kilo(done)} of ~ at ${speed} duration=${format_seconds(duration)}`;
+    }
+    if (is_number_gt(duration, 0)) {
+        return `~ duration=${format_seconds(duration)}`;
+    }
+    return '~';
+}
+
+module.exports = format_progress_kilo;
