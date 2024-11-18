0fc42f07af6854b0c59abd7d8977dff1cb30146f
Vladimir Barbaros <vladimir.b@bannernow.com>
2024-11-18 20:09:00 +0200

    add fs_touch

---
+++ src/fs_touch.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+
+function fs_touch(file)
+{
+    return new Promise(function (resolve, reject) {
+        fs.open(file, 'a', function (error, fd) {
+            if (error) {
+                reject(error);
+                return;
+            }
+            fs.close(fd, function (error) {
+                if (error) {
+                    reject(error);
+                    return;
+                }
+                const now = new Date();
+                fs.utimes(file, now, now, function (error) {
+                    error ? reject(error) : resolve();
+                });
+            });
+        });
+    });
+}
+
+module.exports = fs;
