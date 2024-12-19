d0eccbdb0137b9a0e182356675bfbf05fd8bde43
Vladimir Barbaros <vladimir.b@bannernow.com>
2024-12-19 13:03:59 +0200

    format_kilo: handle number less than 1000

--- src/format_kilo.js
+++ src/format_kilo.js
@@ -8,7 +8,7 @@ function format_kilo(num)
         return '0';
     }
     if (num < 1000) {
-        return num.toString();
+        return num.toFixed(0);
     }
     const i = parseInt(Math.floor(Math.log(num) / Math.log(1000)), 10);
     return `${(num / (1000 ** i)).toFixed(2)}${sizes[i]}`;
