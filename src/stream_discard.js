e078e510cab4507e8c68b94b90b5a0b7536acfd2
Vladimir Barbaros <vladimir.b@bannernow.com>
2024-12-20 19:47:44 +0200

    stream_discard: support objectMode

--- src/stream_discard.js
+++ src/stream_discard.js
@@ -1,8 +1,9 @@
 const stream = require('stream');
 
-function stream_discard()
+function stream_discard({objectMode = false} = {})
 {
     return new stream.Writable({
+        objectMode,
         write: function (buffer, encoding, callback) {
             callback();
         },
