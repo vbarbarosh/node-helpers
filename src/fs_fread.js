fa59c149cdd1d04b8d48e0c58bace75f51c63ac3
Vladimir Barbaros <vladimir.b@bannernow.com>
2024-12-21 21:31:51 +0200

    fs_fread: support for buffer size

--- src/fs_fread.js
+++ src/fs_fread.js
@@ -3,14 +3,14 @@ const fs = require('fs');
 /**
  * The main use case is reading chunks of data from a file.
  */
-async function fs_fread(fp, buffer, offset = null)
+async function fs_fread(fp, buffer, offset = null, size = buffer.length)
 {
     const {bytesRead} = await new Promise(function (resolve, reject) {
-        fs.read(fp, buffer, 0, buffer.length, offset, function (error, out) {
+        fs.read(fp, buffer, 0, size, offset, function (error, out) {
             error ? reject(error) : resolve(out);
         });
     });
-    return (bytesRead === buffer.length) ? buffer : buffer.slice(0, bytesRead);
+    return (bytesRead === buffer.length) ? buffer : buffer.subarray(0, bytesRead);
 }
 
 module.exports = fs_fread;
