f904a0409a4bc396b1a1a77bc8525ad5bd240054
Vladimir Barbaros <vladimir.b@bannernow.com>
2024-12-21 21:21:52 +0200

    fix fs_fopen

--- src/fs_fopen.js
+++ src/fs_fopen.js
@@ -2,7 +2,11 @@ const fs = require('fs');
 
 function fs_fopen(file, flags = 'r', mode = 0o666)
 {
-    return fs.promises.open(file, flags, mode);
+    return new Promise(function (resolve, reject) {
+        fs.open(file, flags, mode, function (error, out) {
+            error ? reject(error) : resolve(out);
+        });
+    });
 }
 
 module.exports = fs_fopen;
