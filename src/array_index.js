2757455f1c225e2a40e1f90d54036f4b5b581159
Vladimir Barbaros <vladimir.b@bannernow.com>
2025-01-24 22:29:47 +0200

    notes

--- src/array_index.js
+++ src/array_index.js
@@ -1,3 +1,10 @@
+/**
+ * Usage:
+ *     array_index(items, v => v.name)
+ *
+ * Seems, there is a native way to do it:
+ *     Object.fromEntries(items.map(v => [v.name, v]));
+ */
 function array_index(array, fn)
 {
     const out = {};
