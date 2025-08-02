bcb72bf70614b505795f3a388ccdf94905a26e5a
Vladimir Barbaros <vladimir.b@bannernow.com>
2025-08-02 09:54:57 +0300

    array_permutations: support for k

--- src/array_permutations.js
+++ src/array_permutations.js
@@ -1,17 +1,21 @@
 // https://stackoverflow.com/a/37580979/23502239
-function array_permutations(array)
+function array_permutations(array, k = array.length)
 {
-    var length = array.length,
-        out = [array.slice()],
-        c = new Array(length).fill(0),
-        i = 1, k, p;
+    if (k !== array.length) {
+        return array_permutations_k(array, k);
+    }
+
+    let length = array.length;
+    let out = [array.slice()];
+    let c = new Array(length).fill(0);
+    let i = 1, kk, p;
 
     while (i < length) {
         if (c[i] < i) {
-            k = i % 2 && c[i];
+            kk = i % 2 && c[i];
             p = array[i];
-            array[i] = array[k];
-            array[k] = p;
+            array[i] = array[kk];
+            array[kk] = p;
             ++c[i];
             i = 1;
             out.push(array.slice());
@@ -19,6 +23,27 @@ function array_permutations(array)
         else {
             c[i] = 0;
             ++i;
+        }
+    }
+    return out;
+}
+
+function array_permutations_k(array, k)
+{
+    if (k === 0) {
+        return [
+            []
+        ];
+    }
+    if (array.length < k) {
+        return [];
+    }
+
+    const out = [];
+    for (let i = 0; i < array.length; i++) {
+        const rest = array.slice(0, i).concat(array.slice(i + 1));
+        for (const perm of array_permutations(rest, k - 1)) {
+            out.push([array[i], ...perm]);
         }
     }
     return out;
--- src/array_permutations.test.js
+++ src/array_permutations.test.js
@@ -7,4 +7,14 @@ describe('array_permutations', function () {
         assert.deepStrictEqual(array_permutations([1,2]), [[1,2],[2,1]]);
         assert.deepStrictEqual(array_permutations([1,2,3]), [[1,2,3],[2,1,3],[3,1,2],[1,3,2],[2,3,1],[3,2,1]]);
     });
+    it('should handle k', function () {
+        assert.deepStrictEqual(array_permutations([1,2], 1), [[1],[2]]);
+        assert.deepStrictEqual(array_permutations([1,2,3], 2), [[1,2],[1,3],[2,1],[2,3],[3,1],[3,2]]);
+        assert.deepStrictEqual(array_permutations([1,2,3,4], 3), [
+            [1,2,3],[1,2,4],[1,3,2],[1,3,4],[1,4,2],[1,4,3],
+            [2,1,3],[2,1,4],[2,3,1],[2,3,4],[2,4,1],[2,4,3],
+            [3,1,2],[3,1,4],[3,2,1],[3,2,4],[3,4,1],[3,4,2],
+            [4,1,2],[4,1,3],[4,2,1],[4,2,3],[4,3,1],[4,3,2]
+        ]);
+    });
 });
