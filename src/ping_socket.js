54fb53f4f36f7f803ad800f6848c8b613a3c4856
Vladimir Barbaros <vladimir.barbarosh@gmail.com>
2025-08-15 11:02:53 +0300

    update ping_socket

--- src/ping_socket.js
+++ src/ping_socket.js
@@ -10,19 +10,21 @@ function ping_socket(socket_path, data = 'PING', timeout_ms = 1000)
 {
     return new Promise(function (resolve, reject) {
         let done = false;
-        const client = net.connect(socket_path);
-        client.setEncoding('utf8');
-        client.setTimeout(timeout_ms, () => finish(new Error('Timeout')));
-        client.on('error', finish);
-        client.on('connect', () => client.end(data));
-        client.on('data', chunk => finish(null, chunk));
-        function finish(error, result) {
+        const buf = [];
+        const socket = net.connect(socket_path);
+        socket.setTimeout(timeout_ms, () => finish(new Error('Socket Timeout')));
+        socket.on('end', () => finish(null, Buffer.concat(buf)))
+        socket.on('error', finish);
+        socket.on('connect', () => socket.end(data));
+        socket.on('data', chunk => buf.push(chunk));
+        function finish(error, out) {
             if (done) {
                 return;
             }
             done = true;
-            client.destroy();
-            error ? reject(error) : resolve(result);
+            socket.removeAllListeners();
+            socket.destroy();
+            error ? reject(error) : resolve(out);
         }
     });
 }
