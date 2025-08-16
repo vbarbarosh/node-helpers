df9d8981232f42905643590d1e727940afb46b6a
Vladimir Barbaros <vladimir.barbarosh@gmail.com>
2025-08-16 19:05:06 +0300

    pid_kill_grace: add log parameter

--- src/pid_kill_grace.js
+++ src/pid_kill_grace.js
@@ -1,8 +1,11 @@
 const Promise = require('bluebird');
+const format_thousands = require('./format_thousands');
+const ignore = require('./ignore');
 const pid_exists = require('./pid_exists');
 
-async function pid_kill_grace(pid, grace_timeout_ms = 5000)
+async function pid_kill_grace(pid, {grace_timeout_ms = 5000, log = ignore} = {})
 {
+    log('Sending SIGTERM');
     if (!process.kill(pid, 'SIGTERM')) {
         throw new Error('SIGTERM Failed');
     }
@@ -12,9 +15,11 @@ async function pid_kill_grace(pid, grace_timeout_ms = 5000)
         if (!pid_exists(pid)) {
             return;
         }
+        log(`Process is still there, waiting for 100ms, ${format_thousands(end - Date.now())}ms left`);
         await Promise.delay(100);
     }
 
+    log('Sending SIGKILL');
     if (!process.kill(pid, 'SIGKILL')) {
         throw new Error('SIGKILL Failed');
     }
