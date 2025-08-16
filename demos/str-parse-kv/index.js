866f473b05cd64cbd68e631787ff858c8a22b0a0
Vladimir Barbaros <vladimir.barbarosh@gmail.com>
2025-08-16 19:10:23 +0300

    add demos/str-parse-kv/index.js

---
+++ demos/str-parse-kv/index.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+const cli = require('../../src/cli');
+const fs_read_utf8 = require('../../src/fs_read_utf8');
+const str_parse_kv = require('../../src/str_parse_kv');
+
+cli(main);
+
+async function main()
+{
+    const utf8 = await fs_read_utf8('/proc/self/status');
+    const kv = Object.fromEntries(utf8.split('\n').map(str_parse_kv));
+    console.log(kv);
+}
