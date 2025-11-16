e54a88c61b85a170012e99ed4413a106f6c0bde3
Vladimir Barbaros <vladimir.barbarosh@gmail.com>
2025-11-16 13:16:34 +0200

    formatting

--- demos/shell-spawn/index.js
+++ demos/shell-spawn/index.js
@@ -10,5 +10,5 @@ async function main()
     // https://test-videos.co.uk/sintel/mp4-h264
     await shell_spawn(['wget', '-O', 'a.mp4', 'https://test-videos.co.uk/vids/sintel/mp4/h264/360/Sintel_360_10s_10MB.mp4'], {stdio: 'inherit'}).promise();
     await shell_spawn(['ffmpeg', '-i', 'a.mp4', '-filter:v', 'setpts=0.2*PTS', '-y', 'b.mp4'], {stdio: 'inherit'}).promise();
-    console.log('done');
+    console.log('🎉 Done');
 }
