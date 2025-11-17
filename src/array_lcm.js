d0002a0e68aa100086fb89ab2fc5e7996a2793bb
Vladimir Barbaros <vladimir.barbarosh@gmail.com>
2025-11-17 10:26:45 +0200

    clean

--- src/array_lcm.js
+++ src/array_lcm.js
@@ -1,6 +1,5 @@
 const math_lcm = require('./math_lcm');
 
-// https://stackoverflow.com/a/39764792/1478566
 function array_lcm(array, fn = Number)
 {
     return array.reduce((a,b) => math_lcm(fn(a), fn(b)));
