f2ca4bf44498ccf2edd8045ddf69a2c59ec73290
Vladimir Barbaros <vladimir.barbarosh@gmail.com>
2025-11-18 08:58:55 +0200

    release v3.72.4

--- docs/js/components/main-demos.js
+++ docs/js/components/main-demos.js
@@ -14,7 +14,7 @@ vue_component('main-demos', {
         demos: function () {
             const package_json = `{
   "dependencies": {
-    "@vbarbarosh/node-helpers": "^3.72.3"
+    "@vbarbarosh/node-helpers": "^3.72.4"
   }
 }`;
             const tmp = {};
