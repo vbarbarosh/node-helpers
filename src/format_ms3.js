5084ded84d19566370c2c7a835176aba8213a288
Vladimir Barbaros <vladimir.barbarosh@gmail.com>
2025-11-24 18:00:16 +0200

    format_ms3: add include_zero_hours param

--- src/format_ms3.js
+++ src/format_ms3.js
@@ -3,7 +3,7 @@ const factorize_ms = require('./factorize_ms');
 /**
  * Format milliseconds
  */
-function format_ms3(ms)
+function format_ms3(ms, include_zero_hours = false)
 {
     if (!ms) {
         ms = 0;
@@ -11,7 +11,7 @@ function format_ms3(ms)
 
     // 00:05.445
     const [h, m, s, xx] = factorize_ms(ms);
-    if (h) {
+    if (h || include_zero_hours) {
         return `0${h}:0${m}:0${s}.00${xx}`.replace(/0+(?=\d\d+[:.]|\d\d\d)/g, '');
     }
     return `0${m}:0${s}.00${xx}`.replace(/0+(?=\d\d+[:.]|\d\d\d)/g, '')
