4a7eb4b07e716bea491253a8d6039d900ac54555
Vladimir Barbaros <vladimir.barbarosh@gmail.com>
2025-11-24 18:39:43 +0200

    fix regex

--- src/browser/index.js
+++ src/browser/index.js
@@ -7,7 +7,7 @@ else {
     window[ns] = {};
     // https://github.com/webpack/webpack/issues/625
     // https://webpack.js.org/guides/dependency-management/#require-context
-    const require_tmp = require.context('..', true, /\b(array|date|http_delete|http_get_blob|http_get_buffer|http_get_json|http_get_utf8|http_head|http_patch_json|http_post_json|http_post_multipart|http_put_buffer|http_put_json|http_put_utf8|identity|ignore|fcmp|filter|format|plural|random_int|urlmod|waitcb)[^/]*(?<!\.test)\.js$/);
+    const require_tmp = require.context('..', true, /^(?!.*(cli-apps|browser|\.d)\/).*\b(array|date|http_delete|http_get_blob|http_get_buffer|http_get_json|http_get_utf8|http_head|http_patch_json|http_post_json|http_post_multipart|http_put_buffer|http_put_json|http_put_utf8|identity|ignore|fcmp|filter|format|plural|random_int|urlmod|waitcb)[^/]*(?<!\.test)\.js$/);
     require_tmp.keys().forEach(function (key) {
         const [, basename] = key.match(/([^/]+)\.js$/);
         window[ns][basename] = require_tmp(key);
